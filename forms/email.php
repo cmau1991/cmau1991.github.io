93490a6286dea08a678766fb8d974c1697e8e00c
cmau1991 <107841625+cmau1991@users.noreply.github.com>
2024-10-06 15:05:01 +0100

    Update email.php

--- forms/email.php
+++ forms/email.php
@@ -4,10 +4,10 @@ require('recaptcha-master/src/autoload.php');
 
 // configure
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Contact form <demo@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'Contact form <charlottenmaughan@hotmail.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
